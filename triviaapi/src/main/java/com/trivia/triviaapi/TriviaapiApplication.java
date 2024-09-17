47e4b33552a2d81566ec92bcfe50a3f63fc29e62
marorcas <ortzmc@gmail.com>
2024-09-18 09:49:09 +1000

    creates setup for backend

---
+++ triviaapi/src/main/java/com/trivia/triviaapi/TriviaapiApplication.java
@@ -0,0 +1,13 @@
+package com.trivia.triviaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaapiApplication.class, args);
+	}
+
+}
---
+++ triviaapi/src/test/java/com/trivia/triviaapi/TriviaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivia.triviaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
