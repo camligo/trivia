bfad1dbaf74644c999109f15c8dd1f138c066133
camligo <camilla.ligovic@gmail.com>
2024-09-18 16:21:09 +1000

    adds testing dependencies

--- triviaapi/src/test/java/com/trivia/triviaapi/TriviaapiApplicationTests.java
+++ triviaapi/src/test/java/com/trivia/triviaapi/TriviaapiApplicationTests.java
@@ -3,11 +3,20 @@ package com.trivia.triviaapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class TriviaapiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+  @Test
+  void simpleTest() {
+    int a = 1;
+    int b = 1;
+    assertThat(a).isEqualTo(b);
+  }
 
 }
