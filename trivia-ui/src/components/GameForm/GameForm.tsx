c5beb5d40d3142b78205819cf7614961b4393b84
camligo <camilla.ligovic@gmail.com>
2024-09-24 18:50:55 +1000

    new: updates styling for create game form

--- trivia-ui/src/components/GameForm/GameForm.tsx
+++ trivia-ui/src/components/GameForm/GameForm.tsx
@@ -3,59 +3,57 @@ import { GameFormData, schema } from "./schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import CategorySelect from "../CategorySelect/CategorySelect";
 import styles from "./GameForm.module.scss";
-import { useState } from "react";
 import { difficulties } from "../../services/trivia-service";
 
 interface GameFormProps {
-    defaultValues?: GameFormData;
-    onSubmit: (data: GameFormData) => Promise<unknown>;
+  defaultValues?: GameFormData;
+  onSubmit: (data: GameFormData) => Promise<unknown>;
 }
 
 const GameForm = ({
-    defaultValues = {category: '9', difficulty: 'easy'},
-    onSubmit
+  defaultValues = {category: '9', difficulty: 'easy'},
+  onSubmit
 }: GameFormProps) => {
 
-    const {
-        reset,
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitSuccessful },
-    } = useForm<GameFormData>({ resolver: zodResolver(schema), defaultValues });
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm<GameFormData>({ resolver: zodResolver(schema), defaultValues });
 
-    const [selectedDifficulty, setSelectedDifficulty] = useState<string>(defaultValues.difficulty);
-
-    const handleClick = async (difficulty: string) => {
-      setSelectedDifficulty(difficulty);
-    }
-
-    isSubmitSuccessful && reset();
+  isSubmitSuccessful && reset();
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className={styles.contentContainer}>
-        <h2 className={styles.heading}>
-          Choose a category:
-        </h2>
-        <CategorySelect register={register("category")} />
-      </div>
-
-      <div className={styles.contentContainer}>
-        <h2 className={styles.heading}>
-          Difficulty level:
-        </h2>
-        <div className={styles.buttonsContainer}>
-          {difficulties.map((level) => (
-            <label key={level} className={styles.btn}>
-              <input
-                {...register("difficulty")}
-                type="radio"
-                value={level.toLowerCase()}
-              />
-              {level.charAt(0).toUpperCase() + level.slice(1)}
-            </label>
-          ))}
+      <div className={styles.formContentContainer}>
+        <div className={styles.contentContainer}>
+          <h2 className={styles.heading}>
+            Choose a category:
+          </h2>
+          <CategorySelect register={register("category")} />
         </div>
+
+        <div className={styles.contentContainer}>
+          <h2 className={styles.heading}>
+            Difficulty level:
+          </h2>
+          <div className={styles.buttonsContainer}>
+            {difficulties.map((level) => (
+              <div key={level}>
+                <input
+                  {...register("difficulty")}
+                  type="radio"
+                  id={level.toLowerCase()}
+                  value={level.toLowerCase()}
+                />
+                <label htmlFor={level.toLowerCase()} className={styles.btn}>
+                  {level.charAt(0).toUpperCase() + level.slice(1)}
+                </label>
+              </div>
+            ))}
+          </div>
+      </div>
       </div>
       <div className={styles.btnContainer}>
         <button className={styles.startBtn} type="submit">
