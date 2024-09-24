a63c2f87f0be8a34603f5daa919c78c00d3f2a6e
camligo <camilla.ligovic@gmail.com>
2024-09-24 14:57:31 +1000

    fixes difficulty buttons to get correct form value

--- trivia-ui/src/components/GameForm/GameForm.tsx
+++ trivia-ui/src/components/GameForm/GameForm.tsx
@@ -33,43 +33,37 @@ const GameForm = ({
     isSubmitSuccessful && reset();
 
   return (
-    <form
-        onSubmit={handleSubmit(onSubmit)}
-    >
-            <div className={styles.contentContainer}>
-                <h2 className={styles.heading}>
-                    Choose a category:
-                </h2>
-                <CategorySelect register={register("category")} />
-            </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.contentContainer}>
+        <h2 className={styles.heading}>
+          Choose a category:
+        </h2>
+        <CategorySelect register={register("category")} />
+      </div>
 
-            <div className={styles.contentContainer}>
-                <h2 className={styles.heading}>
-                Difficulty level:
-                </h2>
-                {/* <DifficultyButtons register={register("difficulty")} /> */}
-
-                <div className={styles.buttonsContainer}>
-                    {difficulties.map((level) => (
-                        <input
-                            key={level}
-                            onClick={() => handleClick(level.toLowerCase())}
-                            className={`${styles.btn} ${
-                                selectedDifficulty === level.toLowerCase() ? styles.selected : ""
-                            }`}
-                            type="radio"
-                            value={level.charAt(0).toUpperCase() + level.slice(1)}
-                        />
-                    ))}
-                </div>
-                <input value={selectedDifficulty} {...register("difficulty")} />
-            </div>
-            <div className={styles.btnContainer}>
-                <button className={styles.startBtn} type="submit">
-                Start
-                </button>
-            </div>
-        </form>
+      <div className={styles.contentContainer}>
+        <h2 className={styles.heading}>
+          Difficulty level:
+        </h2>
+        <div className={styles.buttonsContainer}>
+          {difficulties.map((level) => (
+            <label key={level} className={styles.btn}>
+              <input
+                {...register("difficulty")}
+                type="radio"
+                value={level.toLowerCase()}
+              />
+              {level.charAt(0).toUpperCase() + level.slice(1)}
+            </label>
+          ))}
+        </div>
+      </div>
+      <div className={styles.btnContainer}>
+        <button className={styles.startBtn} type="submit">
+          Start
+        </button>
+      </div>
+    </form>
   )
 }
 
